42f0dfd469218d59deb5e5bfaeada2f6be443b5e
Maximiliano Greco <mgreco@ets.es>
2020-11-03 11:59:39 +0100

    Add dir script

--- docker_run.sh
+++ docker_run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-. ./variables.sh
-MODE=$1
+CDIR=$(cd "$(dirname "$0")"; pwd -P)
+. $CDIR/variables.sh
+
 UUID="$(uuidgen)"
 CONTAINER_NAME="base-${UUID:0:7}"
+DIR=$PWD
 
 docker run \
     --env SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" \
     --env SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" \
     --env CONFIG_INI="$CONFIG_INI" \
     --name $CONTAINER_NAME \
-    --volume $(pwd):/ETS/git/$(basename $PWD) \
+    --volume $(pwd):/ETS/git/$(basename $DIR) \
     --interactive \
     --tty \
     $IMAGE_NAME
--- fix-dns-docker.sh
+++ fix-dns-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # see https://serverfault.com/a/642984/573706
-apt-get install bridge-utils
+# apt-get install bridge-utils
 pkill docker
 iptables -t nat -F
 ifconfig docker0 down
