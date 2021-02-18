08fc27cfab84d4bb900245baf5fa5dc23acf4c43
Maximiliano Greco <mgreco@ets.es>
2021-02-18 12:29:11 +0100

    Fix arguments order

--- variables.sh
+++ variables.sh
@@ -1,8 +1,8 @@
-TAG="0.1.0-py35-pip18-debian"
+PY_VER="3.6.13"
+TAG="0.1.0-py36-pip18-debian"
 IMAGE_NAME="etsgit1.ets.es:5005/com/docker-base:$TAG"
 SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)"
 SSH_PUBLIC_KEY="$(cat ~/.ssh/id_rsa.pub)"
 CONFIG_INI="$(cat /ETS/configs/config.ini)"
-PY_VER="3.5.8"
 APT_LIST=""
 
